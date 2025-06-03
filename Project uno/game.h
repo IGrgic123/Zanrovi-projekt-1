3d6ab0b614dae40ea092cfaa9eada0a7ddae6d4e
IGrgic123 <grgicivan005@gmail.com>
2025-06-03 15:51:43 +0200

    Add files via upload

---
+++ Project uno/game.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdbool.h>
+
+#define FILE_NAME "games.txt"
+#define MAX_NAME_LENGTH 100
+#define MAX_GENRE_LENGTH 50
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    char genre[MAX_GENRE_LENGTH];
+} Game;
+
+typedef enum {
+    MENU_EXIT = 0,
+    MENU_ADD_GAME = 1,
+    MENU_DISPLAY_ALL = 2,
+    MENU_SUGGEST_BY_GENRE = 3
+} MenuOption;
+
+void runMenu(void);
+
+#endif // GAME_H
---
+++ Project uno/main.c
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "game.h"
+
+int main(void) {
+    runMenu();
+    return 0;
+}
+
