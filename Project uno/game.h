5e40c6a9a75ceb359373006dbba56194bac5d797
IGrgic123 <grgicivan005@gmail.com>
2026-06-19 01:46:13 +0200

    Update game.h

--- Project uno/game.h
+++ Project uno/game.h
@@ -3,10 +3,20 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stddef.h>
 
 #define FILE_NAME "games.txt"
+#define TEMP_FILE_NAME "games_temp.txt"
+
 #define MAX_NAME_LENGTH 100
 #define MAX_GENRE_LENGTH 50
+
+#define IS_EMPTY(count) ((count) == 0)
+
+typedef union {
+    int intValue;
+    size_t sizeValue;
+} NumberValue;
 
 typedef struct {
     char name[MAX_NAME_LENGTH];
@@ -23,8 +33,23 @@ enum MenuOption {
     MENU_SUGGEST,
     MENU_DELETE,
     MENU_UPDATE,
-    MENU_SORT
+    MENU_INSERT,
+    MENU_SORT,
+    MENU_SEARCH
 };
+
+static inline void removeNewLine(char* text)
+{
+    if (text != NULL) {
+        while (*text) {
+            if (*text == '\n') {
+                *text = '\0';
+                break;
+            }
+            text++;
+        }
+    }
+}
 
 void runMenu(void);
 
