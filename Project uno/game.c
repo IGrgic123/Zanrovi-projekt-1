c4d6dbda518f30c8b62d963a0096c376dc3f78d0
IGrgic123 <grgicivan005@gmail.com>
2026-06-19 01:47:22 +0200

    Update game.c

--- Project uno/game.c
+++ Project uno/game.c
@@ -8,22 +8,43 @@
 Game* games = NULL;
 size_t gameCount = 0;
 
+static const char tempFileName[] = TEMP_FILE_NAME;
+
 static void loadGamesFromFile(void);
-static void saveGamesToFile(void);
+static bool saveGamesToFile(void);
 static void addGame(void);
 static void displayGames(void);
 static void suggestByGenre(void);
 static void deleteGame(void);
 static void updateGame(void);
+static void insertGame(void);
 static void sortGames(void);
+static void searchGame(void);
 static void clearInputBuffer(void);
+static void freeGames(void);
+
+static bool resizeGames(size_t newCount);
+static bool readLine(char* text, size_t size);
+static bool readMenuChoice(int* choice);
+static bool readSize(size_t* value);
+
 static int compareGames(const void* a, const void* b);
-static void binarySearchGenre(const char* genre, int left, int right);
+static int compareGameKey(const void* key, const void* element);
 static int strcasecmp_custom(const char* s1, const char* s2);
 
+static Game* findGameLinear(const char* name);
+static Game* findGameBsearch(const char* name);
+
+static void binarySearchGenre(const char* genre, int left, int right, bool* found);
+static long getFileSize(FILE* file);
+
 void runMenu(void) {
+    static unsigned int menuOpenCount = 0;
+    int choice = -1;
+
+    menuOpenCount++;
     loadGamesFromFile();
-    int choice;
+
     do {
         printf("\n--- Game Manager ---\n");
         printf("1. Dodaj igru\n");
@@ -31,171 +52,542 @@ void runMenu(void) {
         printf("3. Predlozi po zanru\n");
         printf("4. Obrisi igru\n");
         printf("5. Azuriraj zanr igre\n");
-        printf("6. Sortiraj igre\n");
-        printf("0. Izlaz\nUnesi izbor: ");
+        printf("6. Insert igre na poziciju\n");
+        printf("7. Sortiraj igre\n");
+        printf("8. Pretrazi igru\n");
+        printf("0. Izlaz\n");
+        printf("Unesi izbor: ");
 
-        if (scanf("%d", &choice) != 1) {
-            clearInputBuffer();
+        if (!readMenuChoice(&choice)) {
             printf("Neispravan unos.\n");
             continue;
         }
-        clearInputBuffer();
 
-        switch (choice) {
-        case MENU_ADD: addGame(); break;
-        case MENU_DISPLAY: displayGames(); break;
-        case MENU_SUGGEST: suggestByGenre(); break;
-        case MENU_DELETE: deleteGame(); break;
-        case MENU_UPDATE: updateGame(); break;
-        case MENU_SORT: sortGames(); break;
-        case MENU_EXIT: break;
-        default: printf("Pogresan izbor.\n");
+        switch ((enum MenuOption)choice) {
+        case MENU_ADD:
+            addGame();
+            break;
+        case MENU_DISPLAY:
+            displayGames();
+            break;
+        case MENU_SUGGEST:
+            suggestByGenre();
+            break;
+        case MENU_DELETE:
+            deleteGame();
+            break;
+        case MENU_UPDATE:
+            updateGame();
+            break;
+        case MENU_INSERT:
+            insertGame();
+            break;
+        case MENU_SORT:
+            sortGames();
+            break;
+        case MENU_SEARCH:
+            searchGame();
+            break;
+        case MENU_EXIT:
+            break;
+        default:
+            printf("Pogresan izbor.\n");
+            break;
         }
+
     } while (choice != MENU_EXIT);
 
-    free(games);
-    games = NULL;
+    freeGames();
 }
 
 static void loadGamesFromFile(void) {
     FILE* file = fopen(FILE_NAME, "r");
-    if (!file) {
-        perror("Greska pri otvaranju");
+
+    if (file == NULL) {
+        printf("Datoteka %s nije pronadena: %s\n", FILE_NAME, strerror(errno));
         return;
     }
 
+    if (getFileSize(file) <= 0) {
+        fclose(file);
+        return;
+    }
+
+    rewind(file);
+
     Game temp;
-    while (fscanf(file, " %99[^|]|%49[^\n]\n", temp.name, temp.genre) == 2) {
-        Game* tempPtr = realloc(games, (gameCount + 1) * sizeof(Game));
-        if (!tempPtr) {
-            perror("Greska realloc");
+
+    while (fscanf(file, " %99[^|]|%49[^\n]", temp.name, temp.genre) == 2) {
+        if (!resizeGames(gameCount + 1)) {
             fclose(file);
             return;
         }
-        games = tempPtr;
-        games[gameCount++] = temp;
+
+        games[gameCount] = temp;
+        gameCount++;
     }
+
+    if (ferror(file)) {
+        perror("Greska pri citanju datoteke");
+    }
+
+    if (feof(file)) {
+        clearerr(file);
+    }
+
     fclose(file);
 }
 
-static void saveGamesToFile(void) {
-    FILE* file = fopen(FILE_NAME, "w");
-    if (!file) {
-        perror("Ne mogu pisati u datoteku");
-        return;
+static bool saveGamesToFile(void) {
+    FILE* file = fopen(tempFileName, "w");
+
+    if (file == NULL) {
+        perror("Ne mogu otvoriti privremenu datoteku");
+        return false;
     }
-    for (size_t i = 0; i < gameCount; ++i)
+
+    for (size_t i = 0; i < gameCount; i++) {
         fprintf(file, "%s|%s\n", games[i].name, games[i].genre);
+    }
+
+    if (ferror(file)) {
+        perror("Greska pri pisanju u datoteku");
+        fclose(file);
+        return false;
+    }
+
     fclose(file);
+
+    if (remove(FILE_NAME) != 0) {
+        perror("Greska pri brisanju stare datoteke");
+        remove(tempFileName);
+        return false;
+    }
+
+    if (rename(tempFileName, FILE_NAME) != 0) {
+        perror("Greska pri preimenovanju datoteke");
+        return false;
+    }
+
+    return true;
 }
 
 static void addGame(void) {
     Game newGame;
+
     printf("Ime igre: ");
-    fgets(newGame.name, MAX_NAME_LENGTH, stdin);
-    newGame.name[strcspn(newGame.name, "\n")] = 0;
-
-    printf("Zanr: ");
-    fgets(newGame.genre, MAX_GENRE_LENGTH, stdin);
-    newGame.genre[strcspn(newGame.genre, "\n")] = 0;
-
-    for (size_t i = 0; i < gameCount; ++i) {
-        if (strcasecmp_custom(games[i].name, newGame.name) == 0) {
-            printf("Igra vec postoji.\n");
-            return;
-        }
-    }
-
-    Game* temp = realloc(games, (gameCount + 1) * sizeof(Game));
-    if (!temp) {
-        perror("realloc failed");
+    if (!readLine(newGame.name, MAX_NAME_LENGTH)) {
+        printf("Neispravan unos imena.\n");
         return;
     }
-    games = temp;
-    games[gameCount++] = newGame;
-    saveGamesToFile();
+
+    if (findGameLinear(newGame.name) != NULL) {
+        printf("Igra vec postoji.\n");
+        return;
+    }
+
+    printf("Zanr: ");
+    if (!readLine(newGame.genre, MAX_GENRE_LENGTH)) {
+        printf("Neispravan unos zanra.\n");
+        return;
+    }
+
+    if (!resizeGames(gameCount + 1)) {
+        return;
+    }
+
+    games[gameCount] = newGame;
+    gameCount++;
+
+    if (saveGamesToFile()) {
+        printf("Igra dodana.\n");
+    }
 }
 
 static void displayGames(void) {
-    for (size_t i = 0; i < gameCount; ++i)
-        printf("%zu. %s [%s]\n", i + 1, games[i].name, games[i].genre);
-}
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema spremljenih igara.\n");
+        return;
+    }
 
+    for (size_t i = 0; i < gameCount; i++) {
+        printf("%zu. %s [%s]\n", i + 1, games[i].name, games[i].genre);
+    }
+}
 static void suggestByGenre(void) {
     char genre[MAX_GENRE_LENGTH];
+    bool found = false;
+
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema spremljenih igara.\n");
+        return;
+    }
+
     printf("Unesi zanr: ");
-    fgets(genre, MAX_GENRE_LENGTH, stdin);
-    genre[strcspn(genre, "\n")] = 0;
-    binarySearchGenre(genre, 0, gameCount - 1);
+    if (!readLine(genre, MAX_GENRE_LENGTH)) {
+        printf("Neispravan unos zanra.\n");
+        return;
+    }
+
+    printf("Preporuke za zanr %s:\n", genre);
+    binarySearchGenre(genre, 0, (int)gameCount - 1, &found);
+
+    if (!found) {
+        printf("Nema igre za taj zanr.\n");
+    }
 }
 
 static void deleteGame(void) {
     char name[MAX_NAME_LENGTH];
-    printf("Unesi ime igre za brisanje: ");
-    fgets(name, MAX_NAME_LENGTH, stdin);
-    name[strcspn(name, "\n")] = 0;
 
-    for (size_t i = 0; i < gameCount; ++i) {
-        if (strcasecmp_custom(games[i].name, name) == 0) {
-            for (size_t j = i; j < gameCount - 1; ++j)
-                games[j] = games[j + 1];
-            --gameCount;
-            saveGamesToFile();
-            printf("Igra obrisana.\n");
-            return;
-        }
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema igara za brisanje.\n");
+        return;
     }
-    printf("Nema te igre.\n");
+
+    printf("Unesi ime igre za brisanje: ");
+    if (!readLine(name, MAX_NAME_LENGTH)) {
+        printf("Neispravan unos imena.\n");
+        return;
+    }
+
+    Game* foundGame = findGameLinear(name);
+
+    if (foundGame == NULL) {
+        printf("Nema te igre.\n");
+        return;
+    }
+
+    size_t index = (size_t)(foundGame - games);
+
+    for (size_t i = index; i + 1 < gameCount; i++) {
+        games[i] = games[i + 1];
+    }
+
+    gameCount--;
+
+    if (gameCount == 0) {
+        freeGames();
+    }
+    else if (!resizeGames(gameCount)) {
+        return;
+    }
+
+    if (saveGamesToFile()) {
+        printf("Igra obrisana.\n");
+    }
 }
 
 static void updateGame(void) {
     char name[MAX_NAME_LENGTH];
-    printf("Unesi ime igre za izmjenu: ");
-    fgets(name, MAX_NAME_LENGTH, stdin);
-    name[strcspn(name, "\n")] = 0;
 
-    for (size_t i = 0; i < gameCount; ++i) {
-        if (strcasecmp_custom(games[i].name, name) == 0) {
-            printf("Unesi novi zanr: ");
-            fgets(games[i].genre, MAX_GENRE_LENGTH, stdin);
-            games[i].genre[strcspn(games[i].genre, "\n")] = 0;
-            saveGamesToFile();
-            printf("Zanr azuriran.\n");
-            return;
-        }
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema igara za azuriranje.\n");
+        return;
     }
-    printf("Igra nije nadena.\n");
+
+    printf("Unesi ime igre za izmjenu: ");
+    if (!readLine(name, MAX_NAME_LENGTH)) {
+        printf("Neispravan unos imena.\n");
+        return;
+    }
+
+    Game* foundGame = findGameLinear(name);
+
+    if (foundGame == NULL) {
+        printf("Igra nije nadena.\n");
+        return;
+    }
+
+    printf("Unesi novi zanr: ");
+    if (!readLine(foundGame->genre, MAX_GENRE_LENGTH)) {
+        printf("Neispravan unos zanra.\n");
+        return;
+    }
+
+    if (saveGamesToFile()) {
+        printf("Zanr azuriran.\n");
+    }
+}
+
+static void insertGame(void) {
+    Game newGame;
+    size_t position;
+
+    printf("Unesi poziciju za insert od 1 do %zu: ", gameCount + 1);
+
+    if (!readSize(&position) || position < 1 || position > gameCount + 1) {
+        printf("Neispravna pozicija.\n");
+        return;
+    }
+
+    printf("Ime igre: ");
+    if (!readLine(newGame.name, MAX_NAME_LENGTH)) {
+        printf("Neispravan unos imena.\n");
+        return;
+    }
+
+    if (findGameLinear(newGame.name) != NULL) {
+        printf("Igra vec postoji.\n");
+        return;
+    }
+
+    printf("Zanr: ");
+    if (!readLine(newGame.genre, MAX_GENRE_LENGTH)) {
+        printf("Neispravan unos zanra.\n");
+        return;
+    }
+
+    Game* copiedGame = malloc(sizeof(Game));
+
+    if (copiedGame == NULL) {
+        perror("Greska malloc");
+        return;
+    }
+
+    *copiedGame = newGame;
+
+    if (!resizeGames(gameCount + 1)) {
+        free(copiedGame);
+        copiedGame = NULL;
+        return;
+    }
+
+    for (size_t i = gameCount; i > position - 1; i--) {
+        games[i] = games[i - 1];
+    }
+
+    games[position - 1] = *copiedGame;
+    gameCount++;
+
+    free(copiedGame);
+    copiedGame = NULL;
+
+    if (saveGamesToFile()) {
+        printf("Igra umetnuta.\n");
+    }
 }
 
 static void sortGames(void) {
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema igara za sortiranje.\n");
+        return;
+    }
+
     qsort(games, gameCount, sizeof(Game), compareGames);
-    saveGamesToFile();
-    printf("Sortirano.\n");
+
+    if (saveGamesToFile()) {
+        printf("Sortirano.\n");
+    }
 }
 
-static int compareGames(const void* a, const void* b) {
-    return strcasecmp_custom(((Game*)a)->name, ((Game*)b)->name);
+static void searchGame(void) {
+    char name[MAX_NAME_LENGTH];
+
+    if (IS_EMPTY(gameCount)) {
+        printf("Nema spremljenih igara.\n");
+        return;
+    }
+
+    printf("Unesi ime igre: ");
+    if (!readLine(name, MAX_NAME_LENGTH)) {
+        printf("Neispravan unos imena.\n");
+        return;
+    }
+
+    Game* foundGame = findGameBsearch(name);
+
+    if (foundGame == NULL) {
+        printf("Igra nije pronadena.\n");
+        return;
+    }
+
+    printf("Pronadena igra: %s [%s]\n", foundGame->name, foundGame->genre);
 }
 
-static void binarySearchGenre(const char* genre, int left, int right) {
-    if (left > right) return;
-    int mid = (left + right) / 2;
-    if (strcasecmp_custom(games[mid].genre, genre) == 0)
+static Game* findGameLinear(const char* name) {
+    if (name == NULL || games == NULL) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < gameCount; i++) {
+        if (strcasecmp_custom(games[i].name, name) == 0) {
+            return &games[i];
+        }
+    }
+
+    return NULL;
+}
+static Game* findGameBsearch(const char* name) {
+    if (name == NULL || games == NULL || IS_EMPTY(gameCount)) {
+        return NULL;
+    }
+
+    qsort(games, gameCount, sizeof(Game), compareGames);
+
+    return bsearch(name, games, gameCount, sizeof(Game), compareGameKey);
+}
+
+static void binarySearchGenre(const char* genre, int left, int right, bool* found) {
+    if (genre == NULL || found == NULL || games == NULL || left > right) {
+        return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    binarySearchGenre(genre, left, mid - 1, found);
+
+    if (strcasecmp_custom(games[mid].genre, genre) == 0) {
         printf("Preporuka: %s\n", games[mid].name);
-    binarySearchGenre(genre, left, mid - 1);
-    binarySearchGenre(genre, mid + 1, right);
+        *found = true;
+    }
+
+    binarySearchGenre(genre, mid + 1, right, found);
+}
+
+static bool resizeGames(size_t newCount) {
+    if (newCount == 0) {
+        freeGames();
+        return true;
+    }
+
+    Game* tempPtr = realloc(games, newCount * sizeof(Game));
+
+    if (tempPtr == NULL) {
+        perror("Greska realloc");
+        return false;
+    }
+
+    games = tempPtr;
+    return true;
+}
+
+static void freeGames(void) {
+    if (games != NULL) {
+        free(games);
+        games = NULL;
+    }
+
+    gameCount = 0;
+}
+
+static bool readLine(char* text, size_t size) {
+    if (text == NULL || size == 0) {
+        return false;
+    }
+
+    if (fgets(text, (int)size, stdin) == NULL) {
+        return false;
+    }
+
+    removeNewLine(text);
+
+    return strlen(text) > 0;
+}
+
+static bool readMenuChoice(int* choice) {
+    if (choice == NULL) {
+        return false;
+    }
+
+    NumberValue number;
+
+    if (scanf("%d", &number.intValue) != 1) {
+        clearInputBuffer();
+        return false;
+    }
+
+    clearInputBuffer();
+
+    *choice = number.intValue;
+    return true;
+}
+
+static bool readSize(size_t* value) {
+    if (value == NULL) {
+        return false;
+    }
+
+    NumberValue number;
+
+    if (scanf("%zu", &number.sizeValue) != 1) {
+        clearInputBuffer();
+        return false;
+    }
+
+    clearInputBuffer();
+
+    *value = number.sizeValue;
+    return true;
+}
+
+static long getFileSize(FILE* file) {
+    if (file == NULL) {
+        return -1;
+    }
+
+    if (fseek(file, 0, SEEK_END) != 0) {
+        perror("Greska fseek");
+        return -1;
+    }
+
+    long size = ftell(file);
+
+    if (size == -1L) {
+        perror("Greska ftell");
+        return -1;
+    }
+
+    rewind(file);
+
+    return size;
 }
 
 static void clearInputBuffer(void) {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
+}
+
+static int compareGames(const void* a, const void* b) {
+    if (a == NULL || b == NULL) {
+        return 0;
+    }
+
+    const Game* firstGame = (const Game*)a;
+    const Game* secondGame = (const Game*)b;
+
+    return strcasecmp_custom(firstGame->name, secondGame->name);
+}
+
+static int compareGameKey(const void* key, const void* element) {
+    if (key == NULL || element == NULL) {
+        return 0;
+    }
+
+    const char* name = (const char*)key;
+    const Game* game = (const Game*)element;
+
+    return strcasecmp_custom(name, game->name);
 }
 
 static int strcasecmp_custom(const char* s1, const char* s2) {
-    while (*s1 && *s2) {
-        if (tolower((unsigned char)*s1) != tolower((unsigned char)*s2))
-            return (tolower((unsigned char)*s1) - tolower((unsigned char)*s2));
-        s1++; s2++;
+    if (s1 == NULL || s2 == NULL) {
+        return 0;
     }
-    return (tolower((unsigned char)*s1) - tolower((unsigned char)*s2));
+
+    while (*s1 && *s2) {
+        int first = tolower((unsigned char)*s1);
+        int second = tolower((unsigned char)*s2);
+
+        if (first != second) {
+            return first - second;
+        }
+
+        s1++;
+        s2++;
+    }
+
+    return tolower((unsigned char)*s1) - tolower((unsigned char)*s2);
 }
